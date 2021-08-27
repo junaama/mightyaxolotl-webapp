f87573fff95fbeaec3fa6731501fe3b466423bb0
junaama <0naama0@gmail.com>
2021-08-27 15:56:12 -0700

    update roadmap

--- components/Roadmap.js
+++ components/Roadmap.js
@@ -73,7 +73,7 @@ export default function Roadmap() {
           <div className="order-1 bg-gray-400 rounded-lg shadow-xl w-5/12 px-6 py-4">
             <p className="font-capriola font-bold text-gray-800 text-sm sm:text-lg">
               {" "}
-              Artbook release and charity!
+              Community driven charity support!
             </p>
           </div>
         </div>
