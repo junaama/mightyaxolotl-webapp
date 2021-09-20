4b32f53fb1a6fb0cbd9f1aff785968abd4452e60
junaama <0naama0@gmail.com>
2021-09-20 13:41:58 -0700

    update for SEO

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,35 @@
-import About from "../components/About"
-import Team from "../components/Team"
-import Roadmap from "../components/Roadmap"
-import Accordion from "../components/Accordion"
+import About from "../components/About";
+import Team from "../components/Team";
+import Roadmap from "../components/Roadmap";
+import Accordion from "../components/Accordion";
 import Story from "../components/Story";
 import Sponsor from "../components/Sponsor";
-export default function Test(){
-    return (
-        <>
-        <About/>
-        
-        <Story/>
-        <hr/>
-        <Sponsor/>
-        <hr/>
-        <Roadmap/>
-        <hr/>
-        <Team/>
-        <hr/>
-        <Accordion/>
-        <hr/>
-        </>
-    )
+import Head from "next/head";
+export default function Index() {
+  return (
+    <>
+      <Head>
+        <title>Mighty Axolotl</title>
+        <link rel="icon" type="image/png" href="/favicon.png" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="description"
+          content="Mighty Axolotl is a 10K generative art collection focusing on creating awareness and impact."
+        />
+        <meta property="og:type" content="website" />
+      </Head>
+      <About />
+
+      <Story />
+      <hr />
+      <Sponsor />
+      <hr />
+      <Roadmap />
+      <hr />
+      <Team />
+      <hr />
+      <Accordion />
+      <hr />
+    </>
+  );
 }
