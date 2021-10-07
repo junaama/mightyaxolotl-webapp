4f404b8cabb36169eacbf7de581dc9ce131a52c8
Kooza25 <35069611+Kooza25@users.noreply.github.com>
2021-10-07 20:52:00 +0200

    Update Roadmap.js

--- components/Roadmap.js
+++ components/Roadmap.js
@@ -10,14 +10,14 @@ export default function Roadmap() {
           className="border-2-2 absolute border-opacity-20 border-gray-700 h-full border"
           style={{ left: "50%" }}
         ></div>
-        {/* <!-- right timeline --> */}
-        <div className="mb-8 flex justify-between items-center w-full right-timeline">
+        {/* <!-- left timeline --> */}
+        <div className="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
           <div className="order-1 w-5/12"></div>
           <div className="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-16 h-8 rounded-full">
-            <h1 className="mx-auto font-semibold text-lg text-white">0%</h1>
+            <h1 className="mx-auto text-white font-semibold text-lg">0%</h1>
           </div>
-          <div className="order-1 bg-ma-pink rounded-lg shadow-xl w-5/12 px-6 py-4">
-            <p className="font-capriola font-bold text-white text-sm sm:text-lg ">
+          <div className="order-1 bg-gray-400 rounded-lg shadow-xl w-5/12 px-6 py-4">
+            <p className="font-capriola font-bold text-gray-800 text-sm sm:text-lg ">
               Early adopters sale for early Discord users!
             </p>
           </div>
